0ca8b4bb0dadc352b3eee6812302d4581cff13b1
DrACEnet <104796612+DrACEnet@users.noreply.github.com>
2022-05-27 13:31:01 +0200

    Update backend.tf
    Code personalization - 2

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.1.0"
   backend "remote" {
-    # organization = "Drnet-ACE-test-lab"
+    organization = "Drnet-ACE-test-lab"
     workspaces {
       name = "ace-iac-day-two"
     }
